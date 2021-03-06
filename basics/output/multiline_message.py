979fdc9b3463172b32569fbe84b34e97185c52d3
repl.it user <replituser@example.com>
2021-03-06 17:10:48 +0000

    Added messages to output

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,8 @@
+#Display message to standard output in multiple line of codes 
+
+print("System Failure Imminent!")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting ouput motors")
+print("...rebooting hover engine\n")
+print("System online!")
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 
 print('EA SPorts servers are trash')
+print("First Step : git add .")
+print('Second Step : git commit -m "Your messsage in here"')
+print("Last Step : git push")
