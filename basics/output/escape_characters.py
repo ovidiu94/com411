50c625c12878ac9354a6d9fd4f4bcc65efcc9099
repl.it user <replituser@example.com>
2021-03-06 17:29:07 +0000

    Escape Characters added

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#Dispaying escape characters
+
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Dispay a backslash")
+print("\" Diaplay a double qoute")
+print("\' Dsiplay a single qoute")
