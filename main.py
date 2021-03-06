e7f34fd84768a38daa3b9a07bb55a6afc9b1edcc
repl.it user <replituser@example.com>
2021-03-06 20:14:16 +0000

    While and For loops

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+#While loop (and also For loop) can can be used to have a repetition of a procedure in our code
 
+print("How many times to print the symbol?")
+x =int(input())#x=3
+
+#i is a counter - it keeps track on how many times we went trought the loop 
+i = 0
+
+while i < x:
+  print("\u27bd", i)# condition for repeating the code - as long as i lower than x
+
+  i = i + 1#new value of the counter is one more than it use to be 
+
+print("\nWe left the loop!")
 
 
 
