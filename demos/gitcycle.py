5f912b9b4f8dea2e6748dcde8f1254b38309dbce
repl.it user <replituser@example.com>
2021-03-07 19:54:20 +0000

    GitHub Cycle

--- demos/gitcycle.py
+++ demos/gitcycle.py
@@ -1,7 +1,7 @@
 #GitHub Cycle
 print('EA SPorts servers are trash')
 print("First Step : git add .")
-print('Second Step : git commit -m "Your messsage in here"')
+print('Second Step : git commit -m"Your messsage in here"')
 print("Last Step : git push")
 
 #git pull if is not working we can use git --fetch all
