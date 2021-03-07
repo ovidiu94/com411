038047363fe33c3c055addc7660c752492526907
repl.it user <replituser@example.com>
2021-03-07 16:03:50 +0000

    IF Statement

---
+++ basics/decisions/simple_decision/if_statement.py
@@ -0,0 +1,19 @@
+print("Welcome to the Program!\n")
+
+print("Enter the first number")#ask the user to enter first number
+first_number= input()
+
+print("Enter the second number")#ask the user to enter second number
+second_number=input()
+
+#check whether the first number is bigger than second number.
+#Assume first_number and second_number are existing variables.
+if (first_number > second_number):
+  print("First number is bigger!")
+#check whether the second_number is bigger than first_number.  
+elif(second_number > first_number):
+  print("Second number is bigger!")
+else:
+  print("Both numbers are equal!")  
+print("Done!")#Dispay a message  
+
--- demos/gitcycle.py
+++ demos/gitcycle.py
@@ -3,3 +3,6 @@ print('EA SPorts servers are trash')
 print("First Step : git add .")
 print('Second Step : git commit -m "Your messsage in here"')
 print("Last Step : git push")
+
+#git pull if is not working we can use git --fetch all
+#after we can start the normal gitcycle mention above
