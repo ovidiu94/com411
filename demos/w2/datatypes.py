85cf20c2bfd2431ece47566ecd96d147130469f5
repl.it user <replituser@example.com>
2021-03-07 20:00:33 +0000

    Variables

--- demos/w2/datatypes.py
+++ demos/w2/datatypes.py
@@ -1,7 +1,7 @@
 print("What is your name?")
 #Variable is a container, which can store data for us in the memory (string, integer, float, bool)
 name = input()
-print("What is your age")
+print("What is your age?")
 age = int(input())
 print("What is your bank balance?")
 balance = float(input())
