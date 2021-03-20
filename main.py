cdd85fba231e518295aec2445768b022ce2acb1a
repl.it user <replituser@example.com>
2021-03-20 23:06:40 +0000

    Added code to demonstrate the use of nested loops and decisions

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ print("Please enter the character for the marker:")
 marker = input()
 
 # Find markers
-is_counting = False
-count = 0
+marker1_position = -1
+marker2_position = -1
 
-for character in sequence:
-    if (is_counting == False and character == marker):
-      print("Found first marker")
-      is_counting = True
-    elif (is_counting == True and character == marker):
-      print("Found last marker")
-    elif (is_counting):
-      count += 1
+for position in range(0, len(sequence), 1):
+    letter = sequence[position]
+
+    if (letter == marker):
+        if (marker1_position == -1):
+            marker1_position = position
+        else:
+            marker2_position = position
 
 # Display result
-print(f"The distance between the markers is {count}")
+print(f"The distance between the markers is {marker2_position - marker1_position - 1}.")
 print("Done!")
