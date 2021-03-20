106b44ec47b2e9ad7bcc6d0552e863f100f49936
repl.it user <replituser@example.com>
2021-03-20 18:16:35 +0000

    Added code to demostrate the use of different data types

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -13,15 +13,3 @@ print("How much do you weigh(in kilograms)?")
 weigh = float(input())
 #Note that the bmi can be calculated by dividing the weight (kg) by the height squared.  To work out the square of the height you can multiply the height by itself or use the power operator (**) e.g. height ** 2.
 print("{} you are {} years old and you bmi is {:.2f}".format(name, age, weigh/height**2))
-
-
-
-
-
-
-   
-
-
-
-   
-
