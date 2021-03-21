de254795e9ca10235229fdd3c3bc1872540ba8d6
repl.it user <replituser@example.com>
2021-03-21 17:03:59 +0000

    Code to demonstrate an alternative aproach of nested loops

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ print("Please enter the character for the marker:")
 marker = input()
 
 # Find markers
-marker1_position = -1
-marker2_position = -1
+is_counting = False
+count = 0
 
-for position in range(0, len(sequence), 1):
-    letter = sequence[position]
-
-    if (letter == marker):
-        if (marker1_position == -1):
-            marker1_position = position
-        else:
-            marker2_position = position
+for character in sequence:
+    if (is_counting == False and character == marker):
+      print("Found first marker")
+      is_counting = True
+    elif (is_counting == True and character == marker):
+      print("Found last marker")
+    elif (is_counting):
+      count += 1
 
 # Display result
-print(f"The distance between the markers is {marker2_position - marker1_position - 1}.")
+print(f"The distance between the markers is {count}")
 print("Done!!")
