6b7f6bce47f8bae1f0eef1b4a3f0575a4cd85781
repl.it user <replituser@example.com>
2021-05-25 11:57:20 +0000

    Task 2 Completed

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-def pattern():
-  sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
-  return sequences
+def menu():
+     
+    # Asking the user to choose one of the following options.
+    print("\nPlease choose an option from the menu:\n")
+    # Menu of options
+    print("1-Load Data\n2-Process Data\n3-Visualize Data\n4-Save Data\n5-Exit")
 
-def run():
-  print(pattern())
+    option = int(input())
+    #if..else if...else if...else structure to display the menu of options
+    if option == 1:
+        print("Load Data")
+    elif option == 2:
+        print("Process Data")
+    elif option == 3:
+        print("Visualize Data")
+    elif option == 4:
+        print("Save Data")
+    elif option == 5:
+        print("Exit")
+    else:
+        print("Invalid number for any other option!")
 
-run()
+menu()
---
+++ tui/task1.py
@@ -0,0 +1,5 @@
+#Display a simple welcome message for the user 
+def welcome():
+ print("\n------------------------------Solar Record Management System------------------------------")
+
+welcome()
